6acb54347c46f5b9f8748f80d490e61103f81a22
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2020-09-18 00:43:30 +0900

    로그인 페이지

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let i=0;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>소융봇 로그인페이지</p>
+      <button onClick={()=> {i++; alert(i);}}>click</button>
     </div>
   );
 }
