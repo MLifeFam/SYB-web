3ca3240d078e1aeed3ef450b617d4ea1d371be54
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2020-10-03 01:28:13 +0900

    Updates

---
+++ precache-manifest.608f2e434e0de9a693508953d9c972cd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecaaf76844848e5076314348d233a461",
+    "url": "/SYB-web/index.html"
+  },
+  {
+    "revision": "20567b49aca5b059701f",
+    "url": "/SYB-web/static/css/2.5b5524e9.chunk.css"
+  },
+  {
+    "revision": "b9eecb6d6ac7996163ad",
+    "url": "/SYB-web/static/css/main.3bd70f1e.chunk.css"
+  },
+  {
+    "revision": "20567b49aca5b059701f",
+    "url": "/SYB-web/static/js/2.38d3b903.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/SYB-web/static/js/2.38d3b903.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9eecb6d6ac7996163ad",
+    "url": "/SYB-web/static/js/main.d12e0570.chunk.js"
+  },
+  {
+    "revision": "124577c4a5d3c7622829",
+    "url": "/SYB-web/static/js/runtime-main.56f6806b.js"
+  }
+]);
