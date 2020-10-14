a08e07494b4ece45bd6080f790f6d9c417554b55
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2020-10-15 01:03:55 +0900

    Updates

--- precache-manifest.2107b92607e6fa4257961574a351b8bb.js
+++ precache-manifest.2107b92607e6fa4257961574a351b8bb.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "92409b5bfdf530fc7ed83dcf9759e809",
+    "revision": "617bc6c47d86750a5dd5bd0af963350f",
     "url": "/SYB-web/index.html"
   },
   {
-    "revision": "8fdac45ac8bcf029effd",
+    "revision": "7cda972b934d5bddc0be",
     "url": "/SYB-web/static/css/2.5b5524e9.chunk.css"
   },
   {
-    "revision": "be49c3dc1a80933cc032",
+    "revision": "035cfb7efdbaacf9f1ed",
     "url": "/SYB-web/static/css/main.7cf30ed2.chunk.css"
   },
   {
-    "revision": "8fdac45ac8bcf029effd",
-    "url": "/SYB-web/static/js/2.8b363fd8.chunk.js"
+    "revision": "7cda972b934d5bddc0be",
+    "url": "/SYB-web/static/js/2.3c5b4b36.chunk.js"
   },
   {
     "revision": "2712bb73ae10d07f00f875dd2c841e45",
-    "url": "/SYB-web/static/js/2.8b363fd8.chunk.js.LICENSE.txt"
+    "url": "/SYB-web/static/js/2.3c5b4b36.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "be49c3dc1a80933cc032",
-    "url": "/SYB-web/static/js/main.29102c09.chunk.js"
+    "revision": "035cfb7efdbaacf9f1ed",
+    "url": "/SYB-web/static/js/main.8973461a.chunk.js"
   },
   {
     "revision": "124577c4a5d3c7622829",
