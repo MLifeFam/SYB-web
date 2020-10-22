b2a350c05f211405f6120bf780af2a6333723d5d
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2020-10-23 01:26:17 +0900

    질문 페이지 수정 폼 추가
    1, 질문페이지에서 수정하기 버튼 누르면 modal로 수정 form 띄움
    
    (modal로 페이지를 띄우니 사이트의 가로가 미세하게 변화해서 개선안 필요,
    
    또 modal에 필요없는 버튼이 많아서 다른 방법도 찾아보기)

--- src/pages/Question.js
+++ src/pages/Question.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form, Select, Input, Button,Row, Col, Divider } from "antd";
+import { Form, Select, Input, Button,Row, Col, Divider,Modal } from "antd";
 import axios from "axios";
 import styled from 'styled-components';
 import { ToastContainer, toast } from "react-toastify";
@@ -11,15 +11,80 @@ const Option = Select.Option;
 const { TextArea } = Input;
 
 const List = ({data}) => {
+  const [form] = Form.useForm();
+  const [visible,setVisible] = React.useState(false);
+  const num = data[0];
+  const FormHandler = () => {
+    setVisible(true);
+  };
+
+  const handleOk = (e) => {
+    setVisible(false);
+  };
+
+  const handleCancel = (e) => {
+    setVisible(false);
+  };
+
+  const onFinish = () => {
+
+  }
+
+  const onValuesChange = () => {
+
+  }
+
   return (
     <>
     <Divider orientation="left">{data[0]}</Divider>
-    <Row justify="start" style={{width:"100%"}}>
+    <Row justify="start" style={{width:"90%" , border:"1px solid lightgray",padding:"0.8rem 0",margin:"0 1rem", display:"flex",alignItems:"center",borderRadius:"5px"}}>
       <Col flex={9} style={{marginLeft:"2rem"}}>
         {data[6]}
       </Col>
       <Col flex={1}>
-        {data[7]}
+        <Button onClick={FormHandler}>수정하기</Button>
+        <Modal
+          title="질문수정"
+          visible={visible}
+          onOk={handleOk}
+          onCancel={handleCancel}
+        >
+        <Form form={form} onFinish={onFinish} onFieldsChange={onValuesChange} style={{width:"30rem"}}>
+          <Form.Item label="질문" name="Question" required>
+            <Input defaultValue={data[6]}/>
+          </Form.Item>
+          <Form.Item label="답변" name="Answer" required>
+            <Input defaultValue={data[7]}/>
+          </Form.Item>
+          <Form.Item label="카테고리 1" name="Category1" required>
+            <Input defaultValue={data[1]}/>
+          </Form.Item>
+          <Form.Item label="카테고리 2" name="Category2">
+            <Input/>
+          </Form.Item>
+          <Form.Item label="카테고리 3" name="Category3">
+            <Input/>
+          </Form.Item>
+          <Form.Item label="카테고리 4" name="Category4">
+            <Input/>
+          </Form.Item>
+          <Form.Item label="카테고리 5" name="Category5">
+            <Input/>
+          </Form.Item>
+          <Form.Item label="답변링크" name="Q_link">
+            <Input defaultValue={data[8]}/>
+          </Form.Item>
+          <Form.Item label="이미지링크" name="Img_link">
+            <Input defaultValue={data[9]}/>
+          </Form.Item>
+          
+          <Form.Item colon={false} wrapperCol={{ span: 11, offset: 11 }}>
+            <Button type="primary" icon ={<CloudUploadOutlined />} htmlType="submit">
+              수정하기
+            </Button>
+          </Form.Item>
+        </Form>
+        </Modal>
       </Col>
     </Row>
     </>
