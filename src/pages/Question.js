2da39d42a9ed7c625125bfab8f1073c04814280d
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2020-10-23 00:45:19 +0900

    질문 수정 페이지 데이터 불러오기
    데이터 받아와서 antd Grid Layout으로 표시

--- src/pages/Question.js
+++ src/pages/Question.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Form, Select, Input, Button } from "antd";
+import { Form, Select, Input, Button,Row, Col, Divider } from "antd";
 import axios from "axios";
 import styled from 'styled-components';
 import { ToastContainer, toast } from "react-toastify";
@@ -10,7 +10,31 @@ import { CloudUploadOutlined } from "@ant-design/icons"
 const Option = Select.Option;
 const { TextArea } = Input;
 
-const Status = () => {
+const List = ({data}) => {
+  return (
+    <>
+    <Divider orientation="left">{data[0]}</Divider>
+    <Row justify="start" style={{width:"100%"}}>
+      <Col flex={9} style={{marginLeft:"2rem"}}>
+        {data[6]}
+      </Col>
+      <Col flex={1}>
+        {data[7]}
+      </Col>
+    </Row>
+    </>
+  );
+};
+
+const Question = () => {
+  const [data, setData] = React.useState([]);
+    const getData = React.useCallback(async () => {
+      const response = await axios.get(`https://mfam.site/knowledgePlus/kakao`);
+      setData(response.data.values);
+    }, []);
+    React.useEffect(() => {
+      getData();
+    }, []);
 
   return (
     <div style={{margin: "3% 10%", padding:"1% 0%", display:"flex",alignItems:"center", flexDirection:"column", background:"white", borderRadius:"0.5rem",border:"2px solid lightgray"}}>
@@ -23,8 +47,14 @@ const Status = () => {
       >
         <p >질문 수정 페이지</p>
       </div>
+      {data.reverse().map((it,i)=>{
+        return(
+            <List key = {it[0]} data={it}/>
+        )
+      })}
+      <div style={{marginBottom:"2rem"}}/>
     </div>
   );
 };
 
-export default Status;
+export default Question;
