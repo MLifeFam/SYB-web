d695cdee267e2dd9557db1d3540240f4de22d4f8
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2020-10-23 22:42:26 +0900

    질문수정 페이지 개선
    1. 질문 수정 기능 추가
    2. 질문 삭제 기능 추가
    3. 질문 추가 기능 추가
    4. 질문 번호 프론트에서 세는 방법으로 변경

--- src/pages/Question.js
+++ src/pages/Question.js
@@ -11,6 +11,7 @@ const Option = Select.Option;
 const { TextArea } = Input;
 
 const List = ({data}) => {
+  console.log(data);
   const [form] = Form.useForm();
   const [visible,setVisible] = React.useState(false);
   const num = data[0];
@@ -26,17 +27,50 @@ const List = ({data}) => {
     setVisible(false);
   };
 
-  const onFinish = () => {
+  const onDeleteFunc = async (formData) => {
+    const response = await axios
+      .delete(`https://mfam.site/knowledgePlus/${data[0]}`, formData)
+      .catch((error) => {
+        return toast.error("에러가 났어요!");
+        console.log(error);
+      });
+    toast.success("수정에 성공하였습니다!");
+  };
 
-  }
+  const onChangeFunc = async (formData) => {
+    const response = await axios
+      .put(`https://mfam.site/knowledgePlus/${data[0]}`, formData)
+      .catch((error) => {
+        return toast.error("에러가 났어요!");
+        console.log(error);
+      });
+    toast.success("수정에 성공하였습니다!");
+    console.log(response);
 
-  const onValuesChange = () => {
+  };
 
-  }
+  const onValuesChange = (changedValue, allValue) => {
+    console.log(changedValue);
+  };
+
+  setTimeout(function () {
+    form.setFieldsValue({
+      faqno:data[0],
+      category1: data[1],
+      category2: data[2],
+      category3: data[3],
+      category4: data[4],
+      category5: data[5],
+      question: data[6],
+      questionAnswer: data[7],
+      landingUrl: "",
+      imageinfo: data[9],
+    });
+  },0);
 
   return (
     <>
-    <Divider orientation="left">{data[0]}</Divider>
+    <Divider orientation="left">{data[10]}</Divider>
     <Row justify="start" style={{width:"90%" , border:"1px solid lightgray",padding:"0.8rem 0",margin:"0 1rem", display:"flex",alignItems:"center",borderRadius:"5px"}}>
       <Col flex={9} style={{marginLeft:"2rem"}}>
         {data[6]}
@@ -48,58 +82,112 @@ const List = ({data}) => {
           visible={visible}
           onOk={handleOk}
           onCancel={handleCancel}
+          footer={[
+            null,
+            null,
+          ]}  //ok와 cancel 버튼을 없애기 위함
+          width="40rem"
         >
-        <Form form={form} onFinish={onFinish} onFieldsChange={onValuesChange} style={{width:"30rem"}}>
-          <Form.Item label="질문" name="Question" required>
-            <Input defaultValue={data[6]}/>
+        <Form 
+          form={form} 
+          onFinish={onChangeFunc} 
+          onFieldsChange={onValuesChange} 
+          style={{width:"95%", padding:"0 5%"}}
+        >
+          <Form.Item label="질문" name="question" required>
+            <Input initialvalues={data[6]}/>
           </Form.Item>
-          <Form.Item label="답변" name="Answer" required>
-            <Input defaultValue={data[7]}/>
+          <Form.Item label="답변" name="questionAnswer" required>
+            <Input initialvalues={data[7]}/>
           </Form.Item>
-          <Form.Item label="카테고리 1" name="Category1" required>
-            <Input defaultValue={data[1]}/>
+          <Form.Item label="카테고리 1" name="category1" required>
+            <Input initialvalues={data[1]}/>
           </Form.Item>
-          <Form.Item label="카테고리 2" name="Category2">
-            <Input/>
+          <Form.Item label="카테고리 2" name="category2">
+            <Input initialvalues={data[2]}/>
           </Form.Item>
-          <Form.Item label="카테고리 3" name="Category3">
-            <Input/>
+          <Form.Item label="카테고리 3" name="category3">
+            <Input initialvalues={data[3]}/>
           </Form.Item>
-          <Form.Item label="카테고리 4" name="Category4">
-            <Input/>
+          <Form.Item label="카테고리 4" name="category4">
+            <Input initialvalues={data[4]}/>
           </Form.Item>
-          <Form.Item label="카테고리 5" name="Category5">
-            <Input/>
+          <Form.Item label="카테고리 5" name="category5">
+            <Input initialvalues={data[5]}/>
           </Form.Item>
-          <Form.Item label="답변링크" name="Q_link">
-            <Input defaultValue={data[8]}/>
+          <Form.Item label="답변링크" name="landingUrl">
+            <Input initialvalues={data[8]}/>
           </Form.Item>
-          <Form.Item label="이미지링크" name="Img_link">
-            <Input defaultValue={data[9]}/>
+          <Form.Item label="이미지링크" name="imageinfo">
+            <Input initialvalues={data[9]}/>
           </Form.Item>
           
           <Form.Item colon={false} wrapperCol={{ span: 11, offset: 11 }}>
-            <Button type="primary" icon ={<CloudUploadOutlined />} htmlType="submit">
+            <Button type="primary" icon ={<CloudUploadOutlined />} htmlType="submit" style={{margin:"0 1rem"}}>
               수정하기
             </Button>
           </Form.Item>
+          <Button type="primary" onClick={onDeleteFunc} style={{backgroundColor:"red", color:"white",border:"none"}}>
+            삭제하기
+          </Button>
         </Form>
         </Modal>
       </Col>
     </Row>
+    <ToastContainer
+          position="bottom-right"
+          autoClose={2500}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+      />
     </>
   );
 };
 
 const Question = () => {
+  const [form] = Form.useForm();
+  const [visible,setVisible] = React.useState(false);
   const [data, setData] = React.useState([]);
-    const getData = React.useCallback(async () => {
-      const response = await axios.get(`https://mfam.site/knowledgePlus/kakao`);
-      setData(response.data.values);
-    }, []);
-    React.useEffect(() => {
-      getData();
-    }, []);
+  const num = data[0];
+  const FormHandler = () => {
+    setVisible(true);
+  };
+
+  const handleOk = (e) => {
+    setVisible(false);
+  };
+
+  const handleCancel = (e) => {
+    setVisible(false);
+  };
+
+  const onFinishFunc = async (formData) => {
+    const response = await axios
+      .post(`https://mfam.site/knowledgePlus`, formData)
+      .catch((error) => {
+        return toast.error("에러가 났어요!");
+        console.log(error);
+      });
+    toast.success("수정에 성공하였습니다!");
+    console.log(response);
+  };
+
+  const onValuesChange = (changedValue, allValue) => {
+    console.log(changedValue);
+  };
+ 
+  const getData = React.useCallback(async () => {
+    const response = await axios.get(`https://mfam.site/knowledgePlus/kakao`);
+    setData(response.data.values.reverse());
+  }, []);
+  React.useEffect(() => {
+    getData();
+  }, []);
 
   return (
     <div style={{margin: "3% 10%", padding:"1% 0%", display:"flex",alignItems:"center", flexDirection:"column", background:"white", borderRadius:"0.5rem",border:"2px solid lightgray"}}>
@@ -112,12 +200,68 @@ const Question = () => {
       >
         <p >질문 수정 페이지</p>
       </div>
-      {data.reverse().map((it,i)=>{
+      {data.map((it,i)=>{
+        it[10]=(data.length-i);
         return(
-            <List key = {it[0]} data={it}/>
+            <List key = {i} data={it}/>
         )
       })}
       <div style={{marginBottom:"2rem"}}/>
+      <Button onClick={FormHandler}>
+        질문 추가
+      </Button>
+      <Modal
+          title="질문추가"
+          visible={visible}
+          onOk={handleOk}
+          onCancel={handleCancel}
+          footer={[
+            null,
+            null,
+          ]}  //ok와 cancel 버튼을 없애기 위함
+          width="40rem"
+        >
+        <Form 
+          form={form} 
+          onFinish={onFinishFunc} 
+          onFieldsChange={onValuesChange} 
+          style={{width:"95%", padding:"0 5%"}}
+        >
+          <Form.Item label="질문" name="question" required>
+            <Input/>
+          </Form.Item>
+          <Form.Item label="답변" name="questionAnswer" required>
+            <Input/>
+          </Form.Item>
+          <Form.Item label="카테고리 1" name="category1" required>
+            <Input/>
+          </Form.Item>
+          <Form.Item label="카테고리 2" name="category2">
+            <Input/>
+          </Form.Item>
+          <Form.Item label="카테고리 3" name="category3">
+            <Input/>
+          </Form.Item>
+          <Form.Item label="카테고리 4" name="category4">
+            <Input/>
+          </Form.Item>
+          <Form.Item label="카테고리 5" name="category5">
+            <Input/>
+          </Form.Item>
+          <Form.Item label="답변링크" name="landingUrl">
+            <Input/>
+          </Form.Item>
+          <Form.Item label="이미지링크" name="imageinfo">
+            <Input/>
+          </Form.Item>
+          
+          <Form.Item colon={false} wrapperCol={{ span: 11, offset: 11 }}>
+            <Button type="primary" icon ={<CloudUploadOutlined />} htmlType="submit" style={{margin:"0 1rem"}}>
+              추가하기
+            </Button>
+          </Form.Item>
+        </Form>
+        </Modal>
     </div>
   );
 };
