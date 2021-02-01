e66ca09262e7a0e8ae642b5c6f9996e3a969a3c3
Jongminfire <51112542+Jongminfire@users.noreply.github.com>
2021-02-01 21:41:07 +0900

    관리자페이지 (폴백블록)
    1. 폴백블록을 저장하는 관리자페이지 생성
    2. 폴백블록 삭제시 toast는 여러번 뜨는 오류가 있어 antd notification으로 변경

--- src/pages/Adminpage.js
+++ src/pages/Adminpage.js
@@ -13,7 +13,9 @@ import {
   Image,
   Carousel,
 } from "antd";
+import { Link } from "react-router-dom";
 import axios from "axios";
+import FallbackList from "./Fallbacklist";
 import styled from "styled-components";
 import { ToastContainer, toast } from "react-toastify";
 import { FormInstance } from "antd/lib/form";
@@ -28,17 +30,24 @@ const Option = Select.Option;
 const { TextArea } = Input;
 
 const AdminPage = (props) => {
-  const name = localStorage.getItem("username");
   const role = localStorage.getItem("role");
-  const pageSize = parseInt(window.innerHeight / 50);
+  const department = localStorage.getItem("department");
+  const pageSize = parseInt(window.innerHeight / 70);
+  const [dataSize,setdataSize] = React.useState(0);
   let count = 0;
   const [size, setSize] = React.useState(0);
   // 한 페이지에 담을 데이터 수 (height에 따라 개수 다르게 설정)
   const [form] = Form.useForm();
   const [visible, setVisible] = React.useState(false);
   const [data, setData] = React.useState([]);
-  const [page, setPage] = React.useState(1);
-  const num = data[0];
+  const [page, setPage] = React.useState(0);
+  const token = localStorage.getItem("user_token");
+  const header = {
+    headers: {
+      authorization: `${token}`,
+    },
+  };
+  
   const FormHandler = () => {
     setVisible(true);
   };
@@ -51,42 +60,96 @@ const AdminPage = (props) => {
     setVisible(false);
   };
 
+  const PageRefresh = () => {
+    // const _data = data.slice(
+    //   (count - 1) * pageSize,
+    //   (count - 1) * pageSize + pageSize
+    // );
+    // data page에 따라 자르는 작업
+
+    window.scrollTo({ top: 0, behavior: "smooth" });
+    // data 새로 불러올시 맨 위로 스크롤
+
+    return data.map((it, i) => {
+      // 게시글 번호 계산
+
+      return (
+        <FallbackList
+          key={i}
+          data={it}
+          count={dataSize - i - pageSize * (page)}
+          getData={getData}
+          setPage={setPage}
+          pageSize={pageSize}
+          page={page}
+        />
+      );
+    });
+  };
+
+  const onValuesChange = (changedValue, allValue) => {
+    console.log(changedValue);
+  };
+
+  const onPageChange = (pagenum) => {
+    setPage(pagenum-1);
+  };
+
+  const getData = React.useCallback(async () => {
+    const response = await axios.get(`https://sjswbot.site/fallback?page=${page}&size=${pageSize}`, header, { widthCredentials: true });
+    setdataSize(response.data.result.count);
+    setData(response.data.result.rows);
+    PageRefresh();
+    // setData(response.data.values.reverse());
+  },[page,setPage]);
+
+  React.useEffect(() => {
+    getData();
+  }, [page,setPage]);
+
   return (
       <>
       {role === "2" ? (
         <div
         style={{
-            margin: "3% 10%",
-            padding: "1% 0%",
-            display: "flex",
-            alignItems: "center",
-            flexDirection: "column",
-            background: "white",
-            borderRadius: "0.5rem",
-            border: "2px solid lightgray",
+          margin: "3% 10%",
+          padding: "1% 0%",
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+          background: "white",
+          borderRadius: "0.5rem",
+          border: "2px solid lightgray",
         }}
-        >
+      >
         <div
-            style={{
+          style={{
             textAlign: "center",
             fontSize: "30px",
             fontFamily: "Gothic A1",
-            }}
+          }}
         >
-            <p> 관리자 페이지 </p>
+          <p>관리자 페이지</p>
         </div>
-        <div
-            style={{
-            display: "flex",
-            flexDirection: "column",
-            justifyContent: "center",
-            alignItems: "center",
-            padding: "2rem 2rem",
-            }}
-        >
-            관리자페이지 입니다.
-        </div>
-        </div>
+        <p>챗봇내에서 발화를 인식하지 못한 폴백 문장들입니다.</p>
+        {/* {data.map((it,i)=>{
+          it.count=data.length-i;
+          it.props=props;
+          return(
+              <QuestionList key = {i} data={it} getData={getData}/>
+          )
+        })} */}
+        {PageRefresh()}
+        <div style={{ marginBottom: "2rem" }} />
+        <Pagination
+          current={page+1}
+          total={dataSize}
+          defaultPageSize={pageSize}
+          showSizeChanger={false}
+          onChange={onPageChange}
+          style={{ marginBottom: "1.5rem" }}
+        />
+      </div>
       ):
       <p>관리자만 접근 가능한 페이지 입니다.</p>
       }
---
+++ src/pages/Fallbacklist.js
@@ -0,0 +1,101 @@
+import React, { useEffect } from "react";
+import { Form, Select, Input, Button, Row, Col, Divider,notification, Modal } from "antd";
+import axios from "axios";
+import styled from "styled-components";
+import moment from "moment";
+import { FormInstance } from "antd/lib/form";
+import {
+  CloudUploadOutlined,
+  ExclamationCircleOutlined,
+} from "@ant-design/icons";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+
+const MySwal = withReactContent(Swal);
+const openNotification = (type,comment) => {
+    notification[type]({
+      description: comment,
+      placement: "bottomRight",
+      duration: 1.5,
+      width: "auto",
+    });
+};
+
+const Fallbacklist = ({ data, getData, setPage, page, count ,pageSize}) => {
+  const token = localStorage.getItem("user_token");
+  const header = {
+    headers: {
+      authorization: `${token}`,
+    },
+  };
+
+  const confirmFunc = (formData) => {
+    Swal.fire({
+      title: "삭제하시겠습니까?",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "네",
+      cancelButtonText: "아니요",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        onDeleteFunc(formData);
+      }
+    });
+  };
+
+  const onDeleteFunc = async (formData) => {
+    const response = await axios
+      .delete(`https://sjswbot.site/fallback/${data.idx}`, header, { widthCredentials: true })
+      .catch((error) => {
+        console.log(error);
+        return openNotification('error', '서버와의 에러가 발생했습니다.');
+      });
+    setPage(page);
+    getData();
+    return openNotification('success', '데이터를 삭제했습니다.');
+  };
+
+  return (
+    <>
+      <Divider style={{ margin: "1vh 0" }} />
+      <Row
+        justify="start"
+        style={{
+          width: "90%",
+          border: "1px solid lightgray",
+          padding: "0.8rem 0",
+          margin: "0.1rem 1rem",
+          display: "flex",
+          alignItems: "center",
+          borderRadius: "5px",
+        }}
+      >
+        <Col
+          flex={1}
+          style={{ paddingLeft: "2rem", width: "5%", fontWeight: "bold" }}
+        >
+          {count}
+        </Col>
+        <Col flex={8} style={{ paddingLeft: "2rem", width: "60%" }}>
+          {data.question}
+        </Col>
+        <Col flex={2} style={{ width: "15%" }}>
+          {moment(data.updatedAt).format("LLL")}
+        </Col>
+        <Col flex={1}>
+          <Button
+            onClick={confirmFunc}
+            type="primary"
+            style={{ backgroundColor: "RGB(255,0,0,0.6)", border: "none" }}
+          >
+            삭제하기
+          </Button>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default Fallbacklist;
